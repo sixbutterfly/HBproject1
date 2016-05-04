7bf9983573c3c25544c4d902cfede4396e36a37b
Jhyeong <hyeono2@naver.com>
2016-05-04 14:46:21 +0900

    충돌수정중3

--- Project/src/com/hb/model/grade/GradeDao.java
+++ Project/src/com/hb/model/grade/GradeDao.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.hb.util.DB;
 
@@ -15,7 +16,7 @@ public class GradeDao {
 	String user="scott";
 	String password="tiger";
 	String sql="";
-	GradeDto bean = new GradeDto();
+	
 	Connection conn;
 	PreparedStatement pstmt;
 	ResultSet rs;
@@ -24,25 +25,27 @@ public class GradeDao {
 		conn = DB.getConnection();
 	}
 	
-	public GradeDto list() {
-		sql="select stuno, member.memname, roomno, javagrade, webgrade, framegrade from grade, " +
-			"member where member.memno = (select memno from student)";
+	public ArrayList<GradeDto> list(int roomno) {
+		ArrayList<GradeDto> al = new ArrayList<GradeDto>();
+		sql="SELECT STUDENT.STUNO, MEMBER.MEMNAME, STUDENT.ROOMNO, NVL(GRADE.JAVAGRADE, 0) JAVAGRADE, " +
+			"NVL(GRADE.WEBGRADE, 0) WEBGRADE , NVL(GRADE.FRAMEGRADE, 0) FRAMEGRADE FROM STUDENT, " +
+			"MEMBER, GRADE WHERE MEMBER.MEMNO=STUDENT.MEMNO AND STUDENT.STUNO=GRADE.STUNO " +
+			"AND STUDENT.ROOMNO=? ORDER BY STUNO";
 		try {
-			Class.forName(driver);
-			conn = DriverManager.getConnection(url, user, password);
 			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, roomno);
 			rs = pstmt.executeQuery();
 			while (rs.next()) {
+				GradeDto bean = new GradeDto();
 				bean.setStuno(rs.getInt("stuno"));
 				bean.setMemname(rs.getString("memname"));
 				bean.setRoomno(rs.getInt("roomno"));
 				bean.setJavagrade(rs.getString("javagrade"));
 				bean.setWebgrade(rs.getString("webgrade"));
 				bean.setFramegrade(rs.getString("framegrade"));
+				al.add(bean);
 			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
@@ -53,6 +56,48 @@ public class GradeDao {
 				e.printStackTrace();
 			}
 		}
-		return bean;
+		return al;
+	}
+	
+	public int addGrade(int stuno, String javagrade, String webgrade, String framegrade) {
+		int result=0;
+		sql="UPDATE GRADE SET JAVAGRADE=?, WEBGRADE=?, FRAMEGRADE=? WHERE STUNO=?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, javagrade);
+			pstmt.setString(2, webgrade);
+			pstmt.setString(3, framegrade);
+			pstmt.setInt(4, stuno);
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs!=null) rs.close();
+				if (pstmt!=null) pstmt.close();
+				if (conn!=null) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	public int getRoomno(String id) {
+		int roomno=0;
+		sql="SELECT MEMID, MEMBER.MEMNO, MEMBER.MEMNAME, STUROOM.ROOMNO, TEACHER.TCHNO " +
+			"FROM STUROOM, TEACHER, MEMBER WHERE TEACHER.TCHNO=STUROOM.TCHNO " +
+			"AND MEMBER.MEMNO=TEACHER.MEMNO AND MEMID=?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				roomno = rs.getInt("roomno");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return roomno;
 	}
 }
